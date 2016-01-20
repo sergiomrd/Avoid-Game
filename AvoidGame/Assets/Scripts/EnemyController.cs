5c9dd1db6cbca6f103f94880e3dcfe79a206d9e1
Sergio Malonda <sergio.del93@gmail.com>
2016-01-20 14:31:27 +0100

    Up

---
+++ AvoidGame/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+	public enum BehaviorType {Normal, Faster};
+
+	public BehaviorType enemyBehavior;
+
+	public float speed = 3;
+
+	private bool inPositionStart = false;
+	private bool inPositionEnd = false;
+
+	Vector3 movementVector, spawnVector;
+
+	void Start () {
+	
+		movementVector = new Vector3(0,0f,-1f);
+		spawnVector = new Vector3(0,1f,0f);
+
+	}
+
+	void Awake()
+	{
+		ChooseBehavior();
+	}
+
+	void Update () {
+	
+		if(!inPositionStart)
+		{
+			SpawnMovement();
+		}
+		else if(transform.position.z <= -6)
+		{
+			EndMovement();
+		}
+		else
+		{
+			Vector3 movement = movementVector * speed * Time.deltaTime;
+			transform.position += movement;
+		}
+	}
+
+	void ChooseBehavior()
+	{
+		switch(enemyBehavior)
+		{
+		case BehaviorType.Normal:
+
+			speed = 3;
+
+			break;
+
+		case BehaviorType.Faster:
+
+			speed = 5;
+
+			break;
+		}
+	}
+
+	void SpawnMovement()
+	{
+		if(transform.position.y >= 0.5)
+		{
+			inPositionStart = true;
+		}
+		else
+		{
+			Vector3 movement = spawnVector * speed * Time.deltaTime;
+			transform.position += movement;
+		}
+
+	}
+
+	void EndMovement()
+	{
+		Vector3 movement = -spawnVector * speed * Time.deltaTime;
+		transform.position += movement;
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if(other.CompareTag("Boundary"))
+		{
+			gameObject.SetActive(false);
+		}
+	}
+}
---
+++ AvoidGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 2f;
+	public float acceleration = 0.2f;
+
+
+	Vector3 movementVector;
+
+	void Awake()
+	{
+		movementVector = new Vector3(1f,0,0);
+	}
+
+	void Update () {
+
+
+		Vector3 movement = movementVector * Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+		transform.position += movement;
+
+		if(transform.position.x < -1.47f)
+		{
+			transform.position = new Vector3 (-1.47f, transform.position.y, transform.position.z);
+		}
+		else if (transform.position.x > 2f)
+		{
+			transform.position = new Vector3 (2f, transform.position.y, transform.position.z);
+		}
+			
+	
+	
+	}
+}
