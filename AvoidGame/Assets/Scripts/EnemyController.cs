130c4e370bc6b528a7f0bbea60ff5e42a0ddf672
Sergio Malonda <sergio.del93@gmail.com>
2016-01-21 17:23:24 +0100

    Enemys

--- AvoidGame/Assets/Scripts/EnemyController.cs
+++ AvoidGame/Assets/Scripts/EnemyController.cs
@@ -3,14 +3,19 @@ using System.Collections;
 
 public class EnemyController : MonoBehaviour {
 
-	public enum BehaviorType {Normal, Faster};
+	public enum BehaviorType {Normal, Faster, Rocket};
 
 	public BehaviorType enemyBehavior;
 
 	public float speed = 3;
+	public float acceleration = 4;
+	private float runPosition;
 
-	private bool inPositionStart = false;
-	private bool inPositionEnd = false;
+
+	private bool inPositionStart;
+	private bool inPositionEnd;
+	private bool animationDone;
+	private bool onCorrutineStart;
 
 	Vector3 movementVector, spawnVector;
 
@@ -18,46 +23,158 @@ public class EnemyController : MonoBehaviour {
 	
 		movementVector = new Vector3(0,0f,-1f);
 		spawnVector = new Vector3(0,1f,0f);
+		runPosition = 0.5f;
+		inPositionStart = false;
+		inPositionEnd = false;
+		animationDone = false;
+		onCorrutineStart = false;
 
 	}
 
 	void Awake()
 	{
-		ChooseBehavior();
+		StartingMove(enemyBehavior);
 	}
 
 	void Update () {
-	
+
+		Move();
+
+	}
+		
+	void Move()
+	{
+		//SpawnMovement Up
 		if(!inPositionStart)
 		{
-			SpawnMovement();
+			if(transform.position.y < runPosition)
+			{
+				Vector3 movement = spawnVector * speed * Time.deltaTime;
+				transform.position += movement;
+			}
+			else
+			{
+				inPositionStart = true;
+			}
 		}
-		else if(transform.position.z <= -6)
+
+		//Behavior at Run
+		if(inPositionStart && !inPositionEnd)
 		{
-			EndMovement();
+			if(transform.position.z <= -6)
+			{
+				inPositionEnd = true;
+			}
+			else
+			{
+				DoBehaviorAtRun();
+			}
 		}
-		else
+
+		//End Movement
+		if(inPositionEnd)
 		{
-			Vector3 movement = movementVector * speed * Time.deltaTime;
-			transform.position += movement;
+			Vector3 movement = -spawnVector * speed * Time.deltaTime;
+			transform.position += movement;	
+		}
+			
+
+			
+	}
+
+	void DoBehaviorAtRun()
+	{
+		Vector3 movement;
+
+		switch(enemyBehavior)
+		{
+		case BehaviorType.Normal:
+
+			movement = movementVector * (speed * acceleration) * Time.deltaTime;
+			transform.position += movement;	
+
+			break;
+		case BehaviorType.Faster:
+			
+			movement = movementVector * (speed * acceleration) * Time.deltaTime;
+			transform.position += movement;	
+
+			break;
+		case BehaviorType.Rocket:
+
+			if(!animationDone)
+			{
+
+				if(!onCorrutineStart)
+				{
+					StartCoroutine(RocketAnimation());
+				}
+
+			}
+			else
+			{
+				movement = movementVector * (speed * acceleration) * Time.deltaTime;
+				transform.position += movement;
+			}
+
+			break;
+		
 		}
 	}
 
-	void ChooseBehavior()
+	IEnumerator RocketAnimation()
+	{
+		onCorrutineStart = true;
+
+		while(transform.localScale.x < 0.5 && transform.localScale.y < 0.5 && transform.localScale.z < 0.5)
+		{
+
+			transform.localScale += new Vector3(0.01f,0.01f,0.01f);
+
+			yield return new WaitForSeconds(0.01f);
+
+		}
+				
+		while(transform.localScale.x > 0.2 && transform.localScale.y > 0.2 && transform.localScale.z > 0.2)
+		{
+
+			transform.localScale -= new Vector3(0.01f,0.01f,0.01f);
+
+			yield return new WaitForSeconds(0.01f);
+
+		}
+
+		yield return new WaitForSeconds(0.3f);
+
+		animationDone = true;
+		onCorrutineStart = false;
+	}
+
+	void StartingMove(BehaviorType enemyBehavior)
 	{
 		switch(enemyBehavior)
 		{
 		case BehaviorType.Normal:
 
 			speed = 3;
+			acceleration = 1;
 
 			break;
 
 		case BehaviorType.Faster:
 
-			speed = 5;
+			speed = 4;
+			acceleration = 1;
+			
+			break;
+
+		case BehaviorType.Rocket:
+
+			speed = 3;
+			acceleration = 3;
 
 			break;
+
 		}
 	}
 
@@ -73,12 +190,6 @@ public class EnemyController : MonoBehaviour {
 			transform.position += movement;
 		}
 
-	}
-
-	void EndMovement()
-	{
-		Vector3 movement = -spawnVector * speed * Time.deltaTime;
-		transform.position += movement;
 	}
 
 	void OnTriggerExit(Collider other)
