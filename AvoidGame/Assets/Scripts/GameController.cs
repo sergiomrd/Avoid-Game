a053ceebad7014bf965d9266bb18b86a7213d4eb
sergiomrd <sergio.del93@gmail.com>
2016-01-24 00:05:53 +0100

    Up1

---
+++ AvoidGame/Assets/Scripts/GameController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	public enum GameState {Start, inGame, GameOver};
+
+	public GameState gm;
+
+	public GameObject Menu, GameOver;
+
+	bool pause;
+	float count;
+
+	void Awake()
+	{
+		//gm = GameState.Start;
+		pause = false;
+	}
+
+	void Update () {
+
+		switch (gm) 
+		{
+		case GameState.Start:
+			break;
+
+		case GameState.inGame:
+			
+			if (Input.GetKeyDown (KeyCode.Escape) && !pause) 
+			{
+				pause = true;
+				ShowMenu (Menu,true);
+				Time.timeScale = 0;
+			}
+			else if(Input.GetKeyDown (KeyCode.Escape) && pause)
+			{
+				pause = false;
+				ShowMenu(Menu,false);
+				Time.timeScale = 1;
+			}
+
+			break;
+
+		case GameState.GameOver:
+			
+			ShowMenu (GameOver, true);
+			Time.timeScale = 0;
+
+			break;
+		}
+			
+	
+	}
+	
+	void ShowMenu(GameObject objectToShow, bool choose)
+		{
+			objectToShow.SetActive (choose);
+		}
+
+	public void EndGame()
+	{
+		
+	}
+			
+}
